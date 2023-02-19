0fba96e64e163f7f22a3020ccb36341f7e8a68e7
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-19 19:54:06 -0300

    feat: lógica de login e logout

--- facebook-api/app/Controllers/Http/Auth/Main.ts
+++ facebook-api/app/Controllers/Http/Auth/Main.ts
@@ -1,17 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { StoreValidator } from 'App/Validators/Auth'
 
 export default class AuthController {
-  public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+  public async store({ request, auth }: HttpContextContract) {
+    const { email, password } = await request.validate(StoreValidator)
 
-  public async store({}: HttpContextContract) {}
+    const token = await auth.attempt(email, password, {
+      expiresIn: '30 days',
+    })
 
-  public async show({}: HttpContextContract) {}
+    return token
+  }
 
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ auth }: HttpContextContract) {
+    await auth.logout()
+  }
 }
