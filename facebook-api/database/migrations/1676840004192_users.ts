e128568fcad9fb1e55db6bb1628e69ab1d341612
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-20 15:42:13 -0300

    refactor: remove código desnecessário

--- facebook-api/database/migrations/1676840004192_users.ts
+++ facebook-api/database/migrations/1676840004192_users.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
       table.increments('id').primary()
       table.string('name')
       table.string('username').unique()
-      table.string('email', 255).notNullable().unique()
+      table.string('email').notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
     })
