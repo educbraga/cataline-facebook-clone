4165b6c5a00edefd1bf8424cd9d9e97cf1e95f21
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-20 15:26:42 -0300

    feat: validação do registro do usuário

---
+++ facebook-api/app/Controllers/Http/User/Register.ts
@@ -0,0 +1,24 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { StoreValidator } from 'App/Validators/User/Register'
+import { User } from 'App/Models'
+import faker from 'faker'
+
+export default class UserRegistersController {
+
+  public async store({request}: HttpContextContract) {
+    const { email, redirectUrl } = await request.validate(StoreValidator)
+    const user = await User.create({ email })
+
+    await user.save()
+
+    const key = user.related('keys').create({key: faker.datatype.uuid() + new Date().getTime() })
+
+    const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+
+  }
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+}
