372a34a75eed61a27f75dc0ca5fe48d89fed135e
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-20 15:05:36 -0300

    style: espaço em branco

--- facebook-api/database/migrations/1676850626479_user_keys.ts
+++ facebook-api/database/migrations/1676850626479_user_keys.ts
@@ -3,7 +3,7 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'user_keys'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('key').unique()
@@ -11,7 +11,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
