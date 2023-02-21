8e9702e15c946d99e9afa5b43aa92068c3e95a06
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-21 00:06:42 -0300

    fix: colunas do token no banco de dados

--- facebook-api/database/migrations/1676840004195_api_tokens.ts
+++ facebook-api/database/migrations/1676840004195_api_tokens.ts
@@ -16,6 +16,7 @@ export default class extends BaseSchema {
       */
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
+      table.timestamp('expires_at', { useTz: true })
     })
   }
 
