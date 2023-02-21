63470b9125a3abea71fbc0f503629e5c34a180ab
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-21 00:30:11 -0300

    fix: remove necessidade de ter uma senha durante o pré-cadastro

--- facebook-api/database/migrations/1676840004192_users.ts
+++ facebook-api/database/migrations/1676840004192_users.ts
@@ -9,7 +9,7 @@ export default class extends BaseSchema {
       table.string('name')
       table.string('username').unique()
       table.string('email').notNullable().unique()
-      table.string('password', 180).notNullable()
+      table.string('password', 180)
       table.string('remember_me_token').nullable()
 
       /**
