ae78d7dc75680737a63544ca9f8a110472e099ad
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-24 01:46:41 -0300

    feat: cria controller

---
+++ facebook-api/app/Controllers/Http/Users/ForgotPassword.ts
@@ -0,0 +1,61 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { StoreValidator, UpdateValidator } from 'App/Validators/User/ForgotPassword'
+import { User, UserKey } from 'App/Models'
+import { faker } from '@faker-js/faker'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+export default class ForgotPasswordController {
+  public async store({ request }: HttpContextContract) {
+    await Database.transaction(async (trx) => {
+      const { email, redirectUrl } = await request.validate(StoreValidator)
+      const user = new User()
+
+      user.useTransaction(trx)
+
+      user.email = email
+
+      await user.save()
+
+      const key = faker.datatype.uuid() + new Date().getTime()
+
+      user.related('keys').create({ key })
+
+      const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+
+      // envio do email
+
+      await Mail.send((message) => {
+        message.to(email)
+        message.from('contato@facebook.com', 'Facebook')
+        message.subject('Criação de conta')
+        message.htmlView('emails/forgotPassword', { link })
+      })
+    })
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const userKey = await UserKey.findByOrFail('key', params.key)
+    const user = await userKey.related('user').query().firstOrFail()
+
+    return user
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const { key, name, password } = await request.validate(UpdateValidator)
+
+    const userKey = await UserKey.findByOrFail('key', key)
+
+    const user = await userKey.related('user').query().firstOrFail()
+
+    const username = name.split(' ')[0].toLocaleLowerCase() + new Date().getTime()
+
+    user.merge({ name, password, username })
+
+    await user.save()
+
+    await userKey.delete()
+
+    return response.ok({ message: 'Ok' })
+  }
+}
