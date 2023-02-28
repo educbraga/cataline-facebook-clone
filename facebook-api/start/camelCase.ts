13773b9c49632ffee3ea73e085f5d0c0b39e07dd
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-27 22:32:22 -0300

    feat: Respostas JSON de snake case para camelCase

---
+++ facebook-api/start/camelCase.ts
@@ -0,0 +1,11 @@
+import { string } from '@ioc:Adonis/Core/Helpers'
+import { SnakeCaseNamingStrategy, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+class CamelCaseNamingStrategy extends SnakeCaseNamingStrategy {
+  public serializedName(_model: typeof BaseModel, propertyName: string) {
+    return string.camelCase(propertyName)
+  }
+}
+
+BaseModel.namingStrategy = new CamelCaseNamingStrategy()
+
